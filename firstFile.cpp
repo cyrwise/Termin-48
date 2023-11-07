011c756df1329cac70f4e62c3d0ef53bf22b8507
Alex Chin <alexanderchin@me.com>
2023-11-07 09:34:10 -0800

    formatting cleanup

--- firstFile.cpp
+++ firstFile.cpp
@@ -11,7 +11,12 @@ int main() {
     srand(time(nullptr));
 
     //initializing array of zeros
-    double interface[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+    double interface[4][4] = {
+    {0, 0, 0, 0}, 
+    {0, 0, 0, 0}, 
+    {0, 0, 0, 0}, 
+    {0, 0, 0, 0}
+    };
     
     //displaying interface with a 2
     output_array(interface);
@@ -19,25 +24,24 @@ int main() {
     return 0;
 }
 
-void generate_rand(int& a, int& b){
+void generate_rand(int& a, int& b) {
     //generating random x and y coordinates
     a = rand() % 4;
     b = rand() % 4;
 }
 
-void output_array(double interface[][4]){
+void output_array(double interface[][4]) {
     //calling generating random# function
     int a;
     int b;
     generate_rand(a, b);
     interface[a][b] = 2;
-    for (int i = 0; i<=3; i++){
-        for (int j = 0; j<=3; j++){
+    for (int i = 0; i<=3; i++) {
+        for (int j = 0; j<=3; j++) {
             printf("%.2f\t", interface[i][j]);
         }
         cout << endl;
     }
-
 }
 
 
