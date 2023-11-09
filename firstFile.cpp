171f22eb28f77995500c1adef4ea8bf1628f55d4
Alex Chin <alexanderchin@me.com>
2023-11-08 19:30:57 -0800

    directional input

--- firstFile.cpp
+++ firstFile.cpp
@@ -8,15 +8,53 @@ void output_array(double interface[][4]);
 void generate_rand(int& a, int& b);
 
 int main() {
+    bool isGameActive = true; // Could potentially use this to declare whether the game is active or not. For example if the player has lost yet or not
+
     srand(time(nullptr));
 
     //initializing array of zeros
-    double interface[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-    
+    double interface[4][4] = {
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+    };
+
     //displaying interface with a 2
     output_array(interface);
-    
-    return 0;
+
+    int turns = 0; // TEMPORARY VARIABLE JUST TO MAKE THE PROGRAM STOP ASKING FOR A DIRECTION AFTER 3 OCCURANCES
+    // temporary solution for the player to move in a certain direction
+    do {
+        char direction;
+        cout << "Direction (U, D, L, R): ";
+        cin >> direction;
+
+        // these cout statements can be replaced with other things such as running a funciton that actually executes moves
+        switch(tolower(direction)) {
+        case 'u':
+            cout << "up" << endl;
+            break;
+        case 'd':
+            cout << "down" << endl;
+            break;
+        case 'l':
+            cout << "left" << endl;
+            break;
+        case 'r':
+            cout << "right" << endl;
+            break;
+        // if the user inputs an invalid direction / does not input a character
+        default:
+            cout << "please choose a valid direction" << endl;
+            break;
+        }
+        turns++;
+        if(turns >= 3) {
+            isGameActive = false;
+        }
+    }
+    while(isGameActive == true);
 }
 
 void generate_rand(int& a, int& b){
@@ -37,7 +75,7 @@ void output_array(double interface[][4]){
     //displaying interface
     for (int i = 0; i<=3; i++){
         for (int j = 0; j<=3; j++){
-            printf("%.2f\t", interface[i][j]);
+            printf("%.f\t", interface[i][j]);
         }
         cout << endl;
     }
